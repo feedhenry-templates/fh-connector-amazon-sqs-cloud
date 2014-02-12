ad38cef48f0a4ccb9a8911c4413bccad7bdbc6f3
Cian Clarke <cian@cianclarke.com>
2014-02-12 18:16:09 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,1 +1,14 @@
-null
+/*
+  Returns a list of your queues
+ */
+exports.sqs = function(params, cb){
+  var AWS = require('aws-sdk');
+  var sqs = new AWS.SQS();
+  sqs.listQueues(params, function (err, data) {
+    if (err) {
+      console.log(err); // an error occurred
+    } else {
+      console.log(data); // successful response
+    }
+  });
+};
